4dfd5e96cf3ae991f4cbfa7f78ddad83272e2ee5
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2019-09-04 09:47:53 +0100

    Polynomial functions implemented
    Most polynomial functions have been implemented providing the class with
    core functionality. SHA256 support needs to be included before the
    get_coefficients_as_hash fn can be implemented.

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char** argv) 
 {
     auto polynomial = Polynomial();
-    for(auto coefficient : polynomial.get_coefficients())
-    {
-        std::cout << coefficient << std::endl;
-    }
+    std::cout << "P(X) = " << polynomial.to_string() << std::endl;
+    std::cout << "P(5) = " << polynomial.evaluate(5) << std::endl;
+    std::cout << "Bit string: " << polynomial.get_coefficients_as_bit_string() << std::endl;
+    std::cout << "SHA256: " << polynomial.get_coefficients_as_hash() << std::endl;
 }
--- source_files/polynomial.cpp
+++ source_files/polynomial.cpp
@@ -1,6 +1,8 @@
 #include "../header_files/polynomial.hpp"
 
+#include <bitset>
 #include <random>
+#include <sstream>
 
 Polynomial::Polynomial(unsigned short order) 
 {
@@ -15,12 +17,52 @@ Polynomial::Polynomial(unsigned short order)
     }
 }
 
-unsigned long long Polynomial::evaluate(long indeterminate) { }
+unsigned long long Polynomial::evaluate(long indeterminate) 
+{
+    unsigned long long result = 0;
+    for(auto &coefficient : this->coefficients)
+    {
+        result = result * indeterminate + coefficient;
+    }
+    return result;
+}
 
 std::vector<unsigned short> Polynomial::get_coefficients() { return this->coefficients; }
 
-std::string Polynomial::get_coefficients_as_bit_string() { }
+std::string Polynomial::get_coefficients_as_bit_string() 
+{
+    std::bitset<128> result(*this->coefficients.begin());
+    for(auto iterator = this->coefficients.begin() + 1; iterator != this->coefficients.end(); iterator++)
+    {
+        auto coefficient_bits = std::bitset<128>(*iterator);
+        result = (result << 16 ^ coefficient_bits);
+    }
+    return result.to_string();
+}
 
-std::string Polynomial::get_coefficients_as_hash() { }
+std::string Polynomial::get_coefficients_as_hash() { return std::string(); }
 
-std::string Polynomial::to_string() { }
+std::string Polynomial::to_string() 
+{
+    std::string result;
+    for(auto iterator = this->coefficients.begin(); iterator != this->coefficients.end(); iterator++)
+    {
+        std::stringstream term;
+        std::string coefficient = std::to_string(*iterator);
+        auto exponent = this->coefficients.size() - std::distance(this->coefficients.begin(), iterator) - 1;
+        if(exponent == 0)
+        {
+            term << coefficient;
+        }
+        else if(exponent == 1)
+        {
+            term << coefficient << " * X + ";
+        }
+        else
+        {
+            term << coefficient << " * X^" << exponent << " + ";
+        }
+        result.append(term.str());
+    }
+    return result;
+}
